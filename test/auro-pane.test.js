9c50d5e91b7a157f81f3502a986303a667dbf7d4
Geoff Rich <geoff.rich@alaskaair.com>
2020-09-11 13:59:06 -0700

    feat: add price prop

--- test/auro-pane.test.js
+++ test/auro-pane.test.js
@@ -17,20 +17,36 @@ describe('auro-pane', () => {
     await expect(el).to.be.true;
   });
 
-  const testCases = [
+  const dateTestCases = [
     ["2020-09-09", "Wed Sep 9"],
     ["2020-01-11", "Sat Jan 11"],
     ["invalid date", ""],
   ]
 
-  testCases.forEach(async function ([date, expected]) {
+  dateTestCases.forEach(async function ([date, expected]) {
     it(`parses ${date} correctly`, async () => {
       const el = await fixture(html`
         <auro-pane date=${date}></auro-pane>
-      `)
+      `);
 
       expect(getButtonText(el)).to.equal(expected);
     })
+  })
+
+  it('displays price when provided', async () => {
+    const el = await fixture(html`
+      <auro-pane date="2020-09-09" price="$100"></auro-pane>
+    `);
+
+    expect(getButtonText(el)).to.equal("Wed Sep 9 $100");
+  })
+
+  it('does not display price when disabled', async () => {
+    const el = await fixture(html`
+      <auro-pane date="2020-09-09" price="$100" disabled></auro-pane>
+    `);
+
+    expect(getButtonText(el)).to.equal("Wed Sep 9 ––");
   })
 
 });
