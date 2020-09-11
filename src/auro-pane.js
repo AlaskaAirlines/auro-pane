4b093508ee0f1cac59ba483170a1df1dd420ce35
Geoff Rich <geoff.rich@alaskaair.com>
2020-09-11 10:05:39 -0700

    feat: small styles and date parsing

--- src/auro-pane.js
+++ src/auro-pane.js
@@ -3,31 +3,57 @@
 
 // ---------------------------------------------------------------------
 
-import { LitElement, html, css } from "lit-element";
-import { classMap } from "lit-html/directives/class-map";
+import { LitElement, html, css } from 'lit-element';
+import { classMap } from 'lit-html/directives/class-map';
 
 // Import touch detection lib
-import "focus-visible/dist/focus-visible.min.js";
-import styleCss from "./style-css.js";
+import 'focus-visible/dist/focus-visible.min.js';
+import styleCss from './style-css.js';
 
 // See https://git.io/JJ6SJ for "How to document your components using JSDoc"
 /**
  * auro-pane provides users a way to ...
  *
- * @attr {String} cssClass - Applies designated CSS class to DOM element.
+ * @attr {String} date - Sets date for display. Format should be yyyy-mm-dd.
+ * @attr {Boolean} disabled - Disables the pane and overrides price to be --.
+ * @attr {Boolean} selected - Sets pane state to selected.
  */
 
-// build the component class
 class AuroPane extends LitElement {
-  // constructor() {
-  //   super();
-  // }
+  constructor() {
+    super();
 
-  // function to define props used within the scope of thie component
+    this.weekdays = [
+      'Sun',
+      'Mon',
+      'Tue',
+      'Wed',
+      'Thu',
+      'Fri',
+      'Sat'
+    ];
+
+    this.months = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec'
+    ];
+  }
+
   static get properties() {
     return {
-      selected: { type: Boolean },
-      disabled: { type: Boolean }
+      date: { type: String },
+      disabled: { type: Boolean },
+      selected: { type: Boolean }
     };
   }
 
@@ -37,18 +63,41 @@ class AuroPane extends LitElement {
     `;
   }
 
-  // function that renders the HTML and CSS into  the scope of the component
+  parseDateString() {
+    const dateFormatLength = 10;
+
+    /* eslint-disable no-magic-numbers, sort-vars */
+    if (this.date && this.date.length === dateFormatLength) {
+      // Using substring instead of date parsing due to browser inconsistencies
+      const year = this.date.substring(0, 4),
+        month = this.date.substring(5, 7),
+        date = this.date.substring(8),
+        parsedDate = new Date(year, month - 1, date);
+
+
+      return {
+        day: this.weekdays[parsedDate.getUTCDay()],
+        date: parsedDate.getUTCDate(),
+        month: this.months[parsedDate.getUTCMonth()]
+      };
+    }
+    /* eslint-enable no-magic-numbers, sort-vars */
+
+    return {};
+  }
+
   render() {
     const classes = {
       'auro-pane': true,
       'auro-pane--selected': this.selected,
       'auro-pane--disabled': this.disabled
-    };
+    },
+      parsedDate = this.parseDateString();
 
     return html`
       <button class="${classMap(classes)}" ?disabled="${this.disabled}">
-        <div class="day-of-week">Wed</div>
-        <div class="date">Sep 20</div>
+        <div class="day-of-week">${parsedDate.day}</div>
+        <div class="date">${parsedDate.month} ${parsedDate.date}</div>
       </button>
     `;
   }
@@ -56,6 +105,6 @@ class AuroPane extends LitElement {
 
 /* istanbul ignore else */
 // define the name of the custom component
-if (!customElements.get("auro-pane")) {
-  customElements.define("auro-pane", AuroPane);
+if (!customElements.get('auro-pane')) {
+  customElements.define('auro-pane', AuroPane);
 }
