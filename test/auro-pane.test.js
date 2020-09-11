12f65e8bd65f0955b59ad1cbdff2637ad276bc30
Geoff Rich <geoff.rich@alaskaair.com>
2020-09-11 10:05:39 -0700

    feat: small styles and date parsing

--- test/auro-pane.test.js
+++ test/auro-pane.test.js
@@ -3,17 +3,40 @@ import sinon from 'sinon';
 import '../src/auro-pane.js';
 
 describe('auro-pane', () => {
-  it('auro-pane is accessible', async () => {
+  it('is accessible', async () => {
     const el = await fixture(html`
-      <auro-pane cssclass="testClass"></auro-pane>
+      <auro-pane date="2020-09-09"></auro-pane>
     `);
 
     await expect(el).to.be.accessible();
   });
 
-  it('auro-pane custom element is defined', async () => {
+  it('defines the custom element', async () => {
     const el = await !!customElements.get("auro-pane");
 
     await expect(el).to.be.true;
   });
+
+  const testCases = [
+    ["2020-09-09", "Wed Sep 9"],
+    ["2020-01-11", "Sat Jan 11"],
+    ["invalid date", ""],
+  ]
+
+  testCases.forEach(async function ([date, expected]) {
+    it(`parses ${date} correctly`, async () => {
+      const el = await fixture(html`
+        <auro-pane date=${date}></auro-pane>
+      `)
+
+      expect(getButtonText(el)).to.equal(expected);
+    })
+  })
+
 });
+
+function getButtonText(el) {
+  const button = el.shadowRoot.querySelector('button');
+  const buttonText = button.textContent.replace(/\s+/g, " ").trim();
+  return buttonText;
+}
